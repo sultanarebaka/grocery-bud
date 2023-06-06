c6e08b2d944158ad610b5078746148ec4e4e0b45
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-06-06 10:45:16 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 const form = document.querySelector(".grocery-form");
-
 const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
